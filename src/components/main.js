2f5454972bed43fdc183fb7a3e5bdd905061cb22
marior9 <mariorodriguezan@gmail.com>
2020-03-30 14:34:55 -0400

    Fetch

--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
 export default class Main extends React.Component{
-
+   
+handleUsers=()=>{
+    fetch('http://localhost:3000/api/v1/users', {
+        method: 'GET', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+          })
+          .then((response) => response.json())
+          .then((data) => {
+          console.log('Success:', data);
+          })
+          .catch((error) => {
+          console.error('Error:', error);
+          });
+}
 
    render(){
        return(
            <div class="main">
-               <h1>Hello</h1>
-               
+               <h1>Hello 2</h1>
+               <div>
+                <button onClick={this.handleUsers}>Click</button>
+               </div>
            </div>
        )
    }
