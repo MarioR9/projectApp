88c13bed13f41f850231df5ff8a618fa4fda2964
marior9 <mariorodriguezan@gmail.com>
2020-04-06 13:13:29 -0400

    navBar component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/main.js'
-
+import NavBar from './components/navBar.js'
 
 function App() {
   return (
     <div >
+      <NavBar/>
       <Main/>
     </div>
   );
