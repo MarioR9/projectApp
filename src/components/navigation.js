81857c0a7fcc765a55cf24f36b631ed6eca194b0
marior9 <mariorodriguezan@gmail.com>
2020-07-20 13:20:20 -0400

    border color to navigation bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,24 +6,20 @@ export default class Navigation extends React.Component{
   render(){
     return (
       <div >
-      <Navbar bg="dark"  variant="dark" expand="lg">
+      <Navbar className="nav-color" variant="light" expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" >
-            <NavDropdown title="Categorias" id="basic-nav-dropdown">
-              <NavDropdown title="Deportes" className="nav-color" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/futbol">Futbol</NavDropdown.Item>
+            <NavDropdown  title="Categorias" id="basic-nav-dropdown">
+              <NavDropdown  bg="dark"  variant="dark" expand="lg" title="Deportes" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/futbol" >Futbol</NavDropdown.Item>
               <NavDropdown.Item href="/basket">Basket</NavDropdown.Item>
               <NavDropdown.Item href="/golf">Golf</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown title="Eventos" className="nav-color" id="basic-nav-dropdown">
+            <NavDropdown title="Eventos"  id="basic-nav-dropdown">
               <NavDropdown.Item href="/Cumpleanos">Cumpleanos</NavDropdown.Item>
               <NavDropdown.Item href="/Matrimonio">Matrimonio</NavDropdown.Item>
               <NavDropdown.Item href="/Graduacion">Graduacion</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
             
               <NavDropdown.Divider />
