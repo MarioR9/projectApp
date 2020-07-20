dc71023acefcc55af0f32cd3798197ea33e77be7
marior9 <mariorodriguezan@gmail.com>
2020-07-20 12:56:49 -0400

    categories color added

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,18 +9,23 @@ export default class Navigation extends React.Component{
       <Navbar bg="dark"  variant="dark" expand="lg">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="mr-auto" >
             <NavDropdown title="Categorias" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Deportes</NavDropdown.Item>
-
-              <NavDropdown title="deportes" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/futbol">futbol</NavDropdown.Item>
-              <NavDropdown.Item href="/basket">basket</NavDropdown.Item>
-              <NavDropdown.Item href="/golf">golf</NavDropdown.Item>
+              <NavDropdown title="Deportes" className="nav-color" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/futbol">Futbol</NavDropdown.Item>
+              <NavDropdown.Item href="/basket">Basket</NavDropdown.Item>
+              <NavDropdown.Item href="/golf">Golf</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
-              <NavDropdown.Item href="#action/3.2">Eventos</NavDropdown.Item>
+            <NavDropdown title="Eventos" className="nav-color" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/Cumpleanos">Cumpleanos</NavDropdown.Item>
+              <NavDropdown.Item href="/Matrimonio">Matrimonio</NavDropdown.Item>
+              <NavDropdown.Item href="/Graduacion">Graduacion</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+            
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Noticias</NavDropdown.Item>
             </NavDropdown>
