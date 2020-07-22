4d804ea4a15829b448b540ec6d3eb1e08cf97019
marior9 <mariorodriguezan@gmail.com>
2020-07-22 12:57:53 -0400

    Home Component upcoming events

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Events from './events.js'
-
-import { Container, Row, Col } from 'react-bootstrap';
+import { Card,Button , CardDeck, Container, Row, Col } from 'react-bootstrap';
 
 export default class Home extends React.Component{
    
@@ -12,9 +11,26 @@ export default class Home extends React.Component{
             <Container fluid>
               <Row>
                 <Col>
-               
+               <h2>Proximos eventos</h2>
+               {this.props.events.map(event => 
+                      <CardDeck>
+                      <Card>
+                        <Card.Body>
+                          <Card.Title>{event.title}</Card.Title>
+                          <Card.Text>
+                            {event.description}
+                          </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                          <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                        <Button variant="primary">Ver mas</Button>
+                      </Card>
+                    </CardDeck> 
+                       )}
                 </Col>
                 <Col>
+                <h2>Utilidades</h2>
                 </Col>
               </Row>
             </Container>
