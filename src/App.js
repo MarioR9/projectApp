99af1dbc1f4f84477426bc94a11d1511d8763e17
marior9 <mariorodriguezan@gmail.com>
2020-07-28 12:44:56 -0400

    minor fixes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ import NavBar from './components/navigation.js' //exported component.
 import Noticias from './components/noticias.js'
 import Events from './components/events.js'
 
-
+let names;
+let deporte;
+let href;
 export default class App extends React.Component {
   
   constructor(props) {
@@ -22,9 +24,10 @@ export default class App extends React.Component {
       currentEvent: "",
       events: []};
   }
-
+  
 //fetch all event on load of the page.
 //maybe be better to load on request only. but all at once.
+
   componentDidMount=()=>{
     fetch('http://localhost:3000/api/v1/events', {
       method: 'GET', // or 'PUT'
@@ -39,30 +42,32 @@ export default class App extends React.Component {
   }
 
   handleCategorySelection=(link)=>{
-    this.setState({currentEvent: link})
+    names = this.state.events.map(e => e.deporte)
+    deporte = [...new Set(names)] //remote duplicate for each deporte..
+    href = deporte.find(e => e === link)
+    return href
   }
   
   render(){
+    console.log(this.handleCategorySelection())
     return (
-    <div className="main-screen">
+    <div >
       <div>
       <NavBar events={this.state.events} handleCategorySelection={this.handleCategorySelection}/>
       </div>
       <Router>
-        <div className='main'>
           <Switch>
             <Route exact path='/'><Redirect to="/home" /></Route>
             <Route path='/home'>
             <Home events={this.state.events}/>
             </Route>
-            <Route path={`/${this.state.currentEvent}`}>
-            <Events events={this.state.events} event={this.state.currentEvent}/>
+            <Route path={`/${this.handleCategorySelection()}`}>
+            <Events events={this.state.events} event={this.handleCategorySelection(0)}/>
             </Route>
             <Route path='/noticias'>
             <Noticias events={this.state.events} event={this.state.currentEvent}/>
             </Route>
           </Switch>
-          </div>
       </Router>
       </div>
     
