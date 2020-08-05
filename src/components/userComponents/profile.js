6498de9d8c8ef0d7101d7cfc6599c96de08732ab
marior9 <mariorodriguezan@gmail.com>
2020-08-05 13:53:04 -0400

    profile

--- src/components/userComponents/profile.js
+++ src/components/userComponents/profile.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Card} from 'react-bootstrap';
 
 export default class Profile extends React.Component{
     render(){
         return(
             <div>
-                <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
+                <Card.Title>Detalles</Card.Title>
+                <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+                </Card.Text>
             </div>
         )
     }
