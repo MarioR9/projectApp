43448eabdec34ae6dc854ab89e8d3e583ec5063b
marior9 <mariorodriguezan@gmail.com>
2020-08-05 12:31:29 -0400

    tabs added

--- src/components/user.js
+++ src/components/user.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Tabs , Tab} from 'react-bootstrap';
 
 export default class User extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            key: "home"
+        };
+      }
     render(){
+
         return(
             <div>
-             <h1>USER</h1>
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={this.state.key}
+                onSelect={(k) => this.setState({key: k})}
+                >
+                <Tab eventKey="home" title="Home">
+                </Tab>
+                <Tab eventKey="profile" title="Profile">
+                </Tab>
+                <Tab eventKey="contact" title="Contact" >
+                </Tab>
+            </Tabs>
             </div>
         )
     }
