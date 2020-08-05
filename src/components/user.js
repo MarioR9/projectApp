a18d13ba5881dec75d218c54beda5f13d313b92b
marior9 <mariorodriguezan@gmail.com>
2020-08-05 12:57:15 -0400

    user profile picture and tabs

--- src/components/user.js
+++ src/components/user.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Tabs , Tab} from 'react-bootstrap';
+import { Tabs , Tab, Card, Button, Row, Col, Container} from 'react-bootstrap';
 import Profile from './userComponents/profile.js'
-
+import Edit from './userComponents/edit.js'
 
 export default class User extends React.Component{
     constructor(props) {
@@ -14,7 +14,25 @@ export default class User extends React.Component{
 
         return(
             <div>
-                <div className="container-user">
+                <Container className="container-user">
+                  <Row>
+                    <Col>
+                    <div>
+                <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+                </Card>
+                </div>
+                    </Col>
+                    <Col>
+                    <div>
                 <Tabs
                     id="controlled-tab-example"
                     activeKey={this.state.key}
@@ -24,10 +42,14 @@ export default class User extends React.Component{
                         <Profile/>
                     </Tab>
                     <Tab eventKey="edit" title="Edit" >
+                        <Edit/>
                     </Tab>
                 </Tabs>
                 </div>
-            
+                    </Col>
+                </Row>
+               
+                </Container>
             </div>
         )
     }
