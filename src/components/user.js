c8d7a14e5efde8a5a07064abda5eea2415fc4f9f
marior9 <mariorodriguezan@gmail.com>
2020-08-05 12:35:18 -0400

    user Profile tab minor fixes

--- src/components/user.js
+++ src/components/user.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import { Tabs , Tab} from 'react-bootstrap';
+import Profile from './userComponents/profile.js'
+
 
 export default class User extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            key: "home"
+            key: "Profile"
         };
       }
     render(){
 
         return(
             <div>
-            <Tabs
-                id="controlled-tab-example"
-                activeKey={this.state.key}
-                onSelect={(k) => this.setState({key: k})}
-                >
-                <Tab eventKey="home" title="Home">
-                </Tab>
-                <Tab eventKey="profile" title="Profile">
-                </Tab>
-                <Tab eventKey="contact" title="Contact" >
-                </Tab>
-            </Tabs>
+                <div>
+                <Tabs
+                    id="controlled-tab-example"
+                    activeKey={this.state.key}
+                    onSelect={(k) => this.setState({key: k})}
+                    >
+                    <Tab eventKey="profile" title="Profile">
+                        <Profile/>
+                    </Tab>
+                    <Tab eventKey="edit" title="Edit" >
+                    </Tab>
+                </Tabs>
+                </div>
+            
             </div>
         )
     }
